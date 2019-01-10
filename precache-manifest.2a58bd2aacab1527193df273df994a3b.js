53a8ce2c9c15bd7284bbb338225e2a52297c59fb
Harmanpreet Singh <harman052@gmail.com>
2019-01-10 19:13:12 +0530

    Updates

---
+++ precache-manifest.2a58bd2aacab1527193df273df994a3b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "949587d2f602847afd3f",
+    "url": "/expansion-cards/static/js/runtime~main.949587d2.js"
+  },
+  {
+    "revision": "205ba45303e3dcdaed8b",
+    "url": "/expansion-cards/static/js/main.205ba453.chunk.js"
+  },
+  {
+    "revision": "5557ebc1a253aff6a550",
+    "url": "/expansion-cards/static/js/1.5557ebc1.chunk.js"
+  },
+  {
+    "revision": "205ba45303e3dcdaed8b",
+    "url": "/expansion-cards/static/css/main.e293e6e8.chunk.css"
+  },
+  {
+    "revision": "baa8522addb7c99c42425cd08c1b74b0",
+    "url": "/expansion-cards/index.html"
+  }
+];
